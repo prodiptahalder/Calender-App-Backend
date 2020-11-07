22e1b1afcf2a4349294ac0ee988740b3457180e0
prodiptahalder <haldip9500@gmail.com>
2020-11-07 14:18:31 +0530

    Authentications Added | Prodipta Halder

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.param('userId', getUserById);
 
 //to check user his profile he should be signed in as well as authenticated
 router.get('/user/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 getUser);
 
 //to get all users
@@ -17,32 +17,32 @@ router.get('/users', getAllUsers);
 
 //update user
 router.put('/user/update/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 updateUser);
 
 //Add user goals
 router.put('/user/add/goal/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 addGoal);
 
 //Add user notes
 router.put('/user/add/note/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 addNote);
 
 //Add user keywords
 router.put('/user/add/keyword/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 addKeyword);
 
 //Add user tasks
 router.put('/user/add/task/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 addTask);
 
 //Add user reminders
 router.put('/user/add/reminder/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 addReminder);
 
 /*Invite User to Goal
@@ -53,7 +53,7 @@ body:
         }
     */
 router.put('/user/goal/invite',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 inviteToGoal);
 
 /*Invite User to Task
@@ -64,7 +64,7 @@ body:
         }
     */
    router.put('/user/task/invite',
-//    isSignedIn,isAuthenticated,
+   isSignedIn,isAuthenticated,
    inviteToTask);
 
 /*Check common goals with another User
@@ -74,7 +74,7 @@ body:
         }
  */
 router.post('/user/common/goals/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 commonGoal);
 
 /*Check common tasks with another User
@@ -84,7 +84,7 @@ body:
         }
  */
 router.post('/user/common/tasks/:userId',
-// isSignedIn,isAuthenticated,
+isSignedIn,isAuthenticated,
 commonTask);
 
 module.exports = router;
